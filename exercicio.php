e916d7366d7b430b3da418b3f20c39ec29af569f
Adriano Gonçalves de Souza <adriangonc@yahoo.com.br>
2022-12-07 16:51:53 -0300

    Primeiros testes com php

---
+++ exercicio.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <link rel="stylesheet" href="assets/css/exercicio.css">
+    <title>Exercicios</title>
+</head>
+<body class="exercicio">
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualização do exercicio</h2>
+    </header>
+    <nav class="navegacao">
+        <a href="#" class="verde">Sem formatação</a>
+        <a href="index.php" class="azul">Voltar</a>
+    </nav>
+    <main class="principal">
+        <div class="conteudo">
+            <?php
+                include("{$_GET['dir']}/{$_GET['file']}.php"); //Obter parametros da requisição e concatena
+            ?>
+        </div>
+    </main>
+    <footer class="rodape">
+        adriangong @ <?= date('Y') ?>
+    </footer>
+</body>
+</html>
--- index.php
+++ index.php
@@ -17,7 +17,7 @@
             <div class="modulo verde">
                 <h3>Moludo 1 - Básico</h3>
                 <ul>
-                    <li><a href="#">Exercicio A</a></li>
+                    <li><a href="exercicio.php?dir=teste&file=teste">Exercicio A</a></li>
                     <li><a href="#">Exercicio B</a></li>
                     <li><a href="#">Exercicio C</a></li>
                 </ul>
@@ -50,7 +50,7 @@
         </div>
     </main>
     <footer class="rodape">
-        adriangong @ 2022
+        adriangong @ <?= date('Y') ?>
     </footer>
 </body>
 </html>
