aa8a3d7a352549dd3a13345e7d76a830e87404bf
Adriano Gonçalves de Souza <adriangonc@yahoo.com.br>
2022-12-07 17:24:12 -0300

    Refatoraçao

--- index.php
+++ index.php
@@ -18,34 +18,10 @@
                 <h3>Moludo 1 - Básico</h3>
                 <ul>
                     <li><a href="exercicio.php?dir=teste&file=teste">Exercicio A</a></li>
-                    <li><a href="#">Exercicio B</a></li>
-                    <li><a href="#">Exercicio C</a></li>
+                    
                 </ul>
             </div>
-            <div class="modulo azul">
-                <h3>Moludo 2</h3>
-                <ul>
-                    <li><a href="#">Exercicio A</a></li>
-                    <li><a href="#">Exercicio B</a></li>
-                    <li><a href="#">Exercicio C</a></li>
-                </ul>
-            </div>
-            <div class="modulo amarelo">
-                <h3>Moludo 3</h3>
-                <ul>
-                    <li><a href="#">Exercicio A</a></li>
-                    <li><a href="#">Exercicio B</a></li>
-                    <li><a href="#">Exercicio C</a></li>
-                </ul>
-            </div>
-            <div class="modulo laranja">
-                <h3>Moludo 4</h3>
-                <ul>
-                    <li><a href="#">Exercicio A</a></li>
-                    <li><a href="#">Exercicio B</a></li>
-                    <li><a href="#">Exercicio C</a></li>
-                </ul>
-            </div>
+            
             </nav>
         </div>
     </main>
